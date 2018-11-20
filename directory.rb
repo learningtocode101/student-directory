1fb59172c08e0b852b4e0ca9794be1b2a1f885e3
Shaneil Clarke <shaneilske@yahoo.com>
2018-11-20 14:21:24 +0000

    Put students data in hash instead of array

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 #first we print the list of students
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffery Baratheon",
-  "Norman Bates",
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffery Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
   ]
 # then print a header
 def print_header
@@ -20,7 +20,7 @@ end
 # and then print them
 def print_students(names)
   names.each do |name|
-  puts name
+  puts "#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
 
